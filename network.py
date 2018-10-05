bb628ff093fff637c7a8930468d395b003ff4948
Elias Khsheibun <elias.khsheibun@netlight.com>
2018-10-05 12:22:45 +0200

    lenet

--- network.py
+++ network.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Convolution2D, MaxPooling2D
 
 images = []
 steering_measurement = []
@@ -15,18 +16,29 @@ with open('../drives/drive1/driving_log.csv') as csvfile:
         filename = source_path.split('/')[-1]
         current_path = '../drives/drive1/IMG/' + filename
         image = cv2.imread(current_path)
+        steering_angle = float(line[3])
+
         images.append(image)
-        steering_measurement.append(float(line[3]))
+        steering_measurement.append(steering_angle)
+        images.append(cv2.flip(image,1))
+        steering_measurement.append(-1.0*steering_angle)
+
 
 X_train = np.array(images)
 y_train = np.array(steering_measurement)
 
 model = Sequential()
-
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5,input_shape=(160,320,3)))
+model.add(Convolution2D(6,(5,5),activation="relu"))
+model.add(MaxPooling2D())
+model.add(Convolution2D(6,(5,5),activation="relu"))
+model.add(MaxPooling2D())
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=10)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
